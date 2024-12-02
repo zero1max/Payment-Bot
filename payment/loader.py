15dd17ae5fa4de41b8bd5fdf749c67ee54e6975e
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-12-02 11:52:52 +0500

    Update Commit

--- payment/loader.py
+++ payment/loader.py
@@ -6,8 +6,8 @@ from aiogram.client.default import DefaultBotProperties
 
 dp = Dispatcher()
 db = Database()
-TOKEN = '6450010593:AAF2x15EoyvML4mLFnikZkBQ6uB4BgSW9Dw' #getenv("BOT_TOKEN") # .env faylidan tokenni o'qish
-PROVIDER_TOKEN_CLICK = "398062629:TEST:999999999_F91D8F69C042267444B74CC0B3C747757EB0E065"
+TOKEN = 'BOT_TOKEN' #getenv("BOT_TOKEN") # .env faylidan tokenni o'qish
+PROVIDER_TOKEN_CLICK = "CLICK_TOKEN"
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 router = Router()
 dp.include_router(router)
